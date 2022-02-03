0bace5fed327aa723eac0202f950a15874d4d1e1
sophy <2njoybliss@gmail.com>
2022-02-03 22:43:41 +0900

    css 수정하여 중앙정렬 완성

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -7,23 +7,22 @@ import styled from "styled-components";
 const WrapComponent = styled.div`
 	margin: 0px auto;
 	padding: 0px;
-	display: block;
 	width: 80%;
 	height: 90%;
 `;
 
 const AppLayout = ({ children }) => {
 	return (
-		<div>
+		<>
 			<Header />
 			<WrapComponent>
 				<Row gutter={8}>
-				<Col xs={24} md={12}>
-					{children}
-				</Col>
+					<Col xs={24} md={12}>
+						{children}
+					</Col>
 				</Row>
 			</WrapComponent>
-		</div>
+		</>
 	);
 };
 
