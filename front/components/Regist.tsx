aa211bdbb4c90f9d190cb365173441470204f4e2
sophy <2njoybliss@gmail.com>
2022-02-10 01:08:08 +0900

    회원가입 폼(3) - hook 적용해보기 - 입렵된 값 console 출력

--- front/components/Regist.tsx
+++ front/components/Regist.tsx
@@ -3,8 +3,9 @@ import { Form, Input, Button } from "antd";
 import styled from "styled-components";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
-import useInput from "../hook/useInput";
 import { string } from "prop-types";
+import join from "../reducers/join";
+import useInput from "../hooks/useInput";
 
 const FormStyle = styled(Form)`
 	margin: 0 auto;
@@ -26,28 +27,56 @@ const ButtonStyle = styled.div`
 `;
 
 const Regist = () => {
+	const [userId, onChangeUserId] = useInput(null);
+	const [userEmail, onChangeUserEmail] = useInput(null);
+	const [userPassword, onChangePassword] = useInput(null);
+	const [userName, onChangeUserName] = useInput(null);
+	const [userNickname, onChangeUserNickname] = useInput(null);
+
+	const onSubmit = useCallback(() => {
+		console.log(userId);
+		console.log(userEmail);
+		console.log(userPassword);
+		console.log(userName);
+	}, [userId, userEmail, userPassword]);
 	return (
 		<>
 			<div>
 				<FormStyle
+					onFinish={onSubmit}
 					labelCol={{ span: 2 }}
 					wrapperCol={{ span: 10 }}
 					layout="horizontal"
 				>
 					<FormItemStyle label="아이디">
-						<Input name="user_id" />
+						<Input name="user_id" onChange={onChangeUserId} value={userId} />
 					</FormItemStyle>
 					<FormItemStyle label="이메일">
-						<Input name="email" type="email" required />
+						<Input
+							name="email"
+							type="email"
+							required
+							onChange={onChangeUserEmail}
+							value={userEmail}
+						/>
 					</FormItemStyle>
 					<FormItemStyle label="비밀번호">
-						<Input name="password" type="password" />
+						<Input
+							name="password"
+							type="password"
+							onChange={onChangePassword}
+							value={userPassword}
+						/>
 					</FormItemStyle>
 					<FormItemStyle label="이름">
-						<Input name="name" />
+						<Input name="name" onChange={onChangeUserName} value={userName} />
 					</FormItemStyle>
 					<FormItemStyle label="닉네임">
-						<Input name="nick_name" />
+						<Input
+							name="nick_name"
+							onChange={onChangeUserNickname}
+							value={userNickname}
+						/>
 					</FormItemStyle>
 					<ButtonStyle>
 						<Button style={{ marginRight: "10px" }}>
